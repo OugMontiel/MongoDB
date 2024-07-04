4770b5c397e665719baeb1c9e4fd60e6ff75506d
Diego Alejandro Montiel Florez <88790286+OugMontiel@users.noreply.github.com>
2024-07-04 17:56:51 -0500

    feat: :sparkles: fin de actividad
    Se adapataon todas las consultas

--- PeliculasJs/js/module/movis.js
+++ PeliculasJs/js/module/movis.js
@@ -2,9 +2,9 @@ import { connect } from "../../helpers/db/connect.js";
 
 
 export class movis extends connect {
-    static instance; 
+    static instance;
     constructor() {
-        if(typeof movis.instance === "object") {
+        if (typeof movis.instance === "object") {
             return movis.instance;
         }
         super();
@@ -13,8 +13,137 @@ export class movis extends connect {
         return this;
     }
 
-    async getAllMovis(){
-        let res = await this.collection.find({},{}).toArray();
+    async getAllMovis() {
+        let res = await this.collection.find({}, {}).toArray();
+        return res;
+    }
+
+    async getGenreAccion() {
+        let res = await this.collection.find({
+            'genre': { $eq: 'Accion' }
+        }, {
+            _id: 0, name: 1, genre: 1
+        }).toArray();
+        return res;
+    }
+    async getformatBluray200() {
+        let res = await this.collection.find({
+            format: { $elemMatch: { name: 'Bluray', copies: { $gt: 200 } } }
+        }, {
+            _id: 0, name: 1
+        }).toArray();
+        return res;
+    }
+    async getformatBluray200_2() {
+        let res = await this.collection.find({
+            format: { $elemMatch: { name: 'Bluray', copies: { $gt: 200 } } }
+        }, {
+            _id: 0, name: 1, 'format.copies': 1
+        }).toArray();
+        return res;
+    }
+    async getformatDVD10() {
+        let res = await this.collection.find({
+            format: { $elemMatch: { name: 'dvd', value: { $lt: 10 } } }
+        }, {
+            _id: 0, name: 1
+        }).toArray();
+        return res;
+    }
+    async getCharacterApodoCobb() {
+        let res = await this.collection.find({
+            'character.apodo': 'Cobb'
+        }, {
+            _id: 0, name: 1
+        }).toArray();
+        return res;
+    }
+    async getCharacterActor2AndActor3() {
+        let res = await this.collection.find({
+            'character.id_actor': { $in: [2, 3] }
+        }, {
+            _id: 0, name: 1
+        }).toArray();
+        return res;
+    }
+    async getformatBluray() {
+        let res = await this.collection.find({
+            format: { $elemMatch: { name: 'Bluray' } }
+        }, {
+            _id: 0, name: 1
+        }).toArray();
+        return res;
+    }
+    async getGenreCienciaFiccion() {
+        let res = await this.collection.find({
+            'genre': { $elemMatch: { $eq: 'Ciencia Ficción' } }
+        }, {
+            _id: 0, name: 1, genre: 1
+        }).toArray();
+        return res;
+    }
+    async getCharacterAutorMiguel() {
+        let res = await this.collection.find({
+            character: { $elemMatch: { rol: 'principal', apodo: 'Miguel' } }
+        }, {
+            _id: 0, name: 1
+        }).toArray();
+        return res;
+    }
+    async getFOrmatCopiers100() {
+        let res = await this.collection.find({
+            format: { $elemMatch: { copies: { $gt: 100 } } }
+        }, {
+            _id: 0, name: 1
+        }).toArray();
+        return res;
+    }
+    async getCharacterIdActor1() {
+        let res = await this.collection.find({
+            character: { $elemMatch: { id_actor: { $eq: 1 } } }
+        }, {
+            _id: 0, name: 1
+        }).toArray();
+        return res;
+    }
+    async getCharacterApodoCobb_2() {
+        let res = await this.collection.find({
+            character: { $elemMatch: { rol: 'principal', apodo: 'Cobb' } }
+        }, {
+            _id: 0, name: 1
+        }).toArray();
+        return res;
+    }
+    async getformatBluray200_3() {
+        let res = await this.collection.find({
+            format: { $elemMatch: { name: 'Bluray', copies: { $gt: 200 } } }
+        }, {
+            _id: 0, name: 1
+        }).toArray();
+        return res;
+    }
+    async getformatDVD10_2() {
+        let res = await this.collection.find({
+            format: { $elemMatch: { name: 'dvd', value: { $lt: 10 } } }
+        }, {
+            _id: 0, name: 1
+        }).toArray();
+        return res;
+    }
+    async getCharacterAutorArthur() {
+        let res = await this.collection.find({
+            character: { $elemMatch: { rol: 'secundario', apodo: 'Arthur' } }
+        }, {
+            _id: 0, name: 1
+        }).toArray();
+        return res;
+    }
+    async getCharacterAutorMiguel_2() {
+        let res = await this.collection.find({
+            character: { $elemMatch: { rol: 'principal', apodo: 'Miguel' } }
+        }, {
+            _id: 0, name: 1
+        }).toArray();
         return res;
     }
 }
--- PeliculasJs/main.js
+++ PeliculasJs/main.js
@@ -8,3 +8,20 @@ import { movis } from './js/module/movis.js';
 // console.log(obj2)
 let mongo = new movis()
 console.log(await mongo.getAllMovis());
+
+console.log( await mongo.getGenreAccion() );
+console.log( await mongo.getformatBluray200() );
+console.log( await mongo.getformatBluray200_2() );
+console.log( await mongo.getformatDVD10() );
+console.log( await mongo.getCharacterApodoCobb() );
+console.log( await mongo.getCharacterActor2AndActor3() );
+console.log( await mongo.getformatBluray() );
+console.log( await mongo.getGenreCienciaFiccion() );
+console.log( await mongo.getCharacterAutorMiguel() );
+console.log( await mongo.getFOrmatCopiers100() );
+console.log( await mongo.getCharacterIdActor1() );
+console.log( await mongo.getCharacterApodoCobb_2() );
+console.log( await mongo.getformatBluray200_3() );
+console.log( await mongo.getformatDVD10_2() );
+console.log( await mongo.getCharacterAutorArthur() );
+console.log( await mongo.getCharacterAutorMiguel_2() );
